c06bd90bd4bd0282f40f4a1be1f32d8240b30630
OmarEnriqueVivasEyrich <oevivaseyrich@gmail.com>
2024-10-25 20:51:12 -0500

    Update Reportes.py

--- Reportes.py
+++ Reportes.py
@@ -68,7 +68,7 @@ def generar_reporte_pdf(df, grafica_archivo):
     valor_actual = df['valor'].iloc[0]  # Valor más reciente
     valor_hace_un_dia = df['valor'].iloc[1] if len(df) > 1 else valor_actual
     valor_hace_una_semana = df['valor'].iloc[6] if len(df) > 7 else valor_actual
-    valor_hace_un_mes = df['valor'].iloc[-1] if len(df) > 30 else valor_actual
+    valor_hace_un_mes = df['valor'].iloc[29] if len(df) > 30 else valor_actual
 
     # Calcular estadísticas clave
     max_valor = df['valor'].max()
